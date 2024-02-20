552a00822a43e44056c42ad651993167f5aba93c
Abhay Hitendra Dusane <99988396+CallmeAbhy@users.noreply.github.com>
2024-02-21 01:46:02 +0530

    Delete packages/ui-library directory

--- packages/ui-library/reactivity.js
+++
@@ -1,9 +0,0 @@
-// Inside ui-library/package.js or equivalent
-let currentState = { count: 0 }; // Initial state
-
-// Reactivity method
-export function updateState(newState) {
-  currentState = { ...currentState, ...newState };
-  // Trigger view update using your templating method
-  render(createTemplate(currentState));
-}
